299b4e69dd51702f6658ef2c8a1ff908de1c9df2
Shreesha-08 <Shreesha.082@gmail.com>
2020-11-08 01:44:38 +0530

    spider

---
+++ Assets/Scripts/SpiderMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderMovement : MonoBehaviour
+{
+    float dirY, movespeed = 2f;
+    bool moveUp = true;
+    void FixedUpdate()
+    {
+        if (transform.position.y > 4f)
+            moveUp = false;
+        if (transform.position.y < -0.1f)
+            moveUp = true;
+        if (moveUp)
+            transform.position = new Vector2(transform.position.x, transform.position.y + movespeed * Time.deltaTime);
+        else
+            transform.position = new Vector2(transform.position.x, transform.position.y - movespeed * Time.deltaTime);
+    }
+}
