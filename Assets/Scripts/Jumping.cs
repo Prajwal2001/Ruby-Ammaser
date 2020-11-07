3e287e396dfecec29ab8dd0a4a63d0b8cdc986b7
sza619 <szahere619@gmail.com>
2020-11-07 14:28:36 +0530

    anim3

--- Assets/Scripts/Jumping.cs
+++ Assets/Scripts/Jumping.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Jumping : MonoBehaviour
 {
+    GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = gameObject.transform.parent.gameObject;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.collider.tag == "forJump")
+            player.GetComponent<PlayerMovement>().isGrounded = true;
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "forJump")
+            player.GetComponent<PlayerMovement>().isGrounded = false;
     }
 }
