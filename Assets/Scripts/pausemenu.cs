25a5a86c71c9113058c4225581f7a55cfa44be82
Prajwal2001 <parashuramk237@gmail.com>
2020-11-07 20:36:42 +0530

    .....

---
+++ Assets/Scripts/pausemenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class pausemenu : MonoBehaviour
+{
+    public static bool isgamepaused = false;
+    public GameObject pausemenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(isgamepaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    public void Resume()
+    {
+        pausemenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isgamepaused = false;
+    }
+    void Pause()
+    {
+        pausemenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isgamepaused = true;
+    }
+    public void loadmenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+}
