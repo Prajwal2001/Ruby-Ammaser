0143101631b5a5355b3a4743c0b3bb34a892c5d9
Shreesha-08 <Shreesha.082@gmail.com>
2020-11-07 15:42:35 +0530

    menu

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -5,8 +5,16 @@ using UnityEngine;
 public class MovingPlatform : MonoBehaviour
 {
     float dirY, movespeed = 3f;
-    void Update()
+    bool moveUp = true;
+    void FixedUpdate()
     {
-        
+        if (transform.position.y > 3y)
+                moveUp = false;
+        if (transform.position.y < -4y)
+                moveUp = true;
+        if (moveUp)
+            transform.position = new Vector2(transform.position.x, transform.position.y + movespeed * Time.deltaTime);
+        else
+            transform.position = new Vector2(transform.position.x, transform.position.y - movespeed * Time.deltaTime);
     }
 }
