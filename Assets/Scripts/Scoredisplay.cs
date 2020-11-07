d75f43202d4dabc2b1df47655ccd60a97a5c0766
Shreesha-08 <Shreesha.082@gmail.com>
2020-11-07 23:38:00 +0530

    ntg

---
+++ Assets/Scripts/Scoredisplay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Scoredisplay : MonoBehaviour
+{
+    public static Scoredisplay instance;
+    public TextMeshProUGUI text;
+    int score;
+    void Start()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+    public void ChangeScore(int rubyvalue)
+    {
+        score = rubyvalue;
+        text.text = "X" + score.ToString();
+    }
+}
