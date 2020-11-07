a673415f47d871da5f8c4e8a8a49e96e93e3ff2b
Prajwal2001 <parashuramk237@gmail.com>
2020-11-07 15:43:18 +0530

    ....

---
+++ Assets/Scripts/backtomainmenu.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class backtomainmenu : MonoBehaviour
+{
+    public void btmm()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
