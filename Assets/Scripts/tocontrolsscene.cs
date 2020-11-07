77cb30ecb8092005cde91627f47d6adce621e6e2
Prajwal2001 <parashuramk237@gmail.com>
2020-11-07 21:05:39 +0530

    buttons

---
+++ Assets/Scripts/tocontrolsscene.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class tocontrolsscene : MonoBehaviour
+{
+    public void tocontrols()
+    {
+        SceneManager.LoadScene("HelpMenu2");
+    }
+}
---
+++ Assets/Scripts/tohelp.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class tohelp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Tohelp()
+    {
+        SceneManager.LoadScene("HelpMenu");
+    }
+}
