301d5fd816882947887602592fa926796e482a13
Shreesha-08 <Shreesha.082@gmail.com>
2020-11-08 00:19:19 +0530

    jump

--- Assets/Scripts/Jumping.cs
+++ Assets/Scripts/Jumping.cs
@@ -13,12 +13,12 @@ public class Jumping : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.collider.tag == "forJump")
+        if (collision.collider.tag == "forJumping")
             player.GetComponent<PlayerMovement>().isGrounded = true;
     }
     private void OnCollisionExit2D(Collision2D collision)
     {
-        if (collision.collider.tag == "forJump")
+        if (collision.collider.tag == "forJumping")
             player.GetComponent<PlayerMovement>().isGrounded = false;
     }
 }
