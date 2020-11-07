3e593420b1fbb65ebb211bcded6b61a09595bb04
Prajwal2001 <parashuramk237@gmail.com>
2020-11-07 23:35:15 +0530

    scoring

--- Assets/Scripts/Scoredisplay.cs
+++ Assets/Scripts/Scoredisplay.cs
@@ -1,18 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Scoredisplay : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static Scoredisplay instance;
+    public TextMeshProUGUI text;
+    int score;
     void Start()
     {
-        
+        if(instance == null)
+        {
+            instance = this;
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+    public void ChangeScore(int rubyvalue)
     {
-        
+        score = rubyvalue;
+        text.text = "X" + score.ToString();
     }
 }
---
+++ Assets/Scripts/rubyscript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rubyscript : MonoBehaviour
+{
+    public int rubyval = 1;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Scoredisplay.instance.ChangeScore(rubyval);
+            Destroy(gameObject);
+        }
+    }
+}
