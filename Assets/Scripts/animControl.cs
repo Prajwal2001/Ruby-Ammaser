e5a845353a78be46a171c9103750aad2c9b78c41
sza619 <szahere619@gmail.com>
2020-11-07 21:38:33 +0530

    anim added!!! finally xD

---
+++ Assets/Scripts/animControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+        { anim.SetBool("isWalking", true); }
+        else
+            anim.SetBool("isWalking", false);
+    }
+}
