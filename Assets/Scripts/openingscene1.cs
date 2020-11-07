f8d63535e5ca34d54d54a1918aaeb43f0586aff1
Prajwal2001 <parashuramk237@gmail.com>
2020-11-07 18:14:18 +0530

    first scene

--- Assets/Scripts/openingscene1.cs
+++ Assets/Scripts/openingscene1.cs
@@ -1,18 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class openingscene1 : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    public void stscreen()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+            SceneManager.LoadScene("MainMenu");
         
     }
 }
