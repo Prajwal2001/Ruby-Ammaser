34030f665604f25592aa8713925de52bf4d4000a
Shreesha-08 <Shreesha.082@gmail.com>
2020-11-08 09:14:49 +0530

    move

---
+++ Assets/Scripts/Ghost2Move.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ghost2Move : MonoBehaviour
+{
+    float dirX, movespeed = 1f;
+    bool moveRight = true;
+    void FixedUpdate()
+    {
+        if (transform.position.x > 249f)
+            moveRight = false;
+        if (transform.position.x < 241f)
+            moveRight = true;
+        if (moveRight)
+            transform.position = new Vector2(transform.position.x + movespeed * Time.deltaTime, transform.position.y);
+        else
+            transform.position = new Vector2(transform.position.x - movespeed * Time.deltaTime, transform.position.y);
+    }
+}
