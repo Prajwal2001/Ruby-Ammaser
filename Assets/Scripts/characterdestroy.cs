312072e1206578ba661bbdd8599be62493524af5
Prajwal2001 <parashuramk237@gmail.com>
2020-11-08 09:56:20 +0530

    ....

--- Assets/Scripts/characterdestroy.cs
+++ Assets/Scripts/characterdestroy.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using 
 public class characterdestroy : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
--- Assets/Scripts/tonextlevel.cs
+++ Assets/Scripts/tonextlevel.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.SocialPlatforms.Impl;
 public class tonextlevel : MonoBehaviour
 {
+    
     private void OnCollisionEnter2D(Collision2D collision)
     {
+       
         if (collision.gameObject.tag == "door")
         {
             
